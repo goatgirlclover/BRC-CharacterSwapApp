2c8ff1ffe9936007367c367c71c2b9c715014cc3
clover <76261419+goatgirlclover@users.noreply.github.com>
2025-07-09 01:51:19 -0700

    Bump version to 1.2.1

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -16,7 +16,7 @@ using CommonAPI;
 
 namespace CharacterSwapApp; 
 
-[BepInPlugin("goatgirl.CharacterSwapApp", "CharacterSwapApp", "1.2.0")]
+[BepInPlugin("goatgirl.CharacterSwapApp", "CharacterSwapApp", "1.2.1")]
 [BepInProcess("Bomb Rush Cyberfunk.exe")]
 [BepInDependency("CommonAPI", BepInDependency.DependencyFlags.HardDependency)]
 [BepInDependency("CrewBoom", BepInDependency.DependencyFlags.SoftDependency)]
